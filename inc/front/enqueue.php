b741ddb0ed1664209cf46b8762f6deb06fd055dd
Juan Malagon <nicolas0915@hotmail.com>
2021-07-07 12:17:22 -0500

    Cache for CSS and JS

--- inc/front/enqueue.php
+++ inc/front/enqueue.php
@@ -2,18 +2,19 @@
 
 function an_enqueue(){
     $uri = get_theme_file_uri();
+    $ver = AN_DEV_MODE ? time() : false;
 
     wp_register_style('an_fonts', $uri . '/assets/css/fonts.css');
     wp_register_style('bootstrap', $uri . '/assets/css/bootstrap/css/bootstrap.min.css');
-    wp_register_style('an_style', $uri . '/assets/css/style.css');
+    wp_register_style('an_style', $uri . '/assets/css/style.css', [], $ver);
     wp_register_style('swiper', $uri . '/assets/css/swiper/swiper-bundle.min.css');
 
     wp_enqueue_style( 'bootstrap' );
     wp_enqueue_style( 'an_fonts' );
     wp_enqueue_style( 'an_style' );
 
-    wp_register_script( 'an_js', $uri . '/assets/js/index.js', [], false, true );
-    wp_register_script('swiper_js', $uri . '/assets/js/swiper-bundle.min.js', [], false, true);
+    wp_register_script( 'an_js', $uri . '/assets/js/index.js', [], $ver, true );
+    wp_register_script( 'swiper_js', $uri . '/assets/js/swiper-bundle.min.js', [], false, true);
 
     if(is_page('tu-luz-en-movimiento')){
         wp_enqueue_style('swiper');
